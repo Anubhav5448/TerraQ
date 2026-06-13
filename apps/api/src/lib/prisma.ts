31a0e6fd94bb004ab7f715855aef3ac478fed9d5
Anubhav yadav <anubhav1374@gmail.com>
2026-06-14 00:06:35 +0530

    6

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,8 +1,4 @@
-import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
