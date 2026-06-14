1a8ad29d07b82a5474e9668cfb44c2156918df3c
Anubhav yadav <anubhav1374@gmail.com>
2026-06-14 16:06:25 +0530

    Notification

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   turbopack: {},
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
